45802da3841a6bc58cf2f6b56b19229eee059313
beepboop-lettuce <ericaharris0486@gmail.com>
2022-01-10 12:12:11 -0600

    added notes for what this loop is doing

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+//three part for loop. Useful for executing something a set number of times.
+//three parts separated by semicolons. First part = where your index begins.
+//second part = condition that allows you to continue in that loop. third =
+//modifies your increment for whatever value you want.
 func main() {
 	for i := 10; i >= 0; i-- {
 		fmt.Println("i is", i)
